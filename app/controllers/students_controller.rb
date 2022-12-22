86415a7258dc08132d75621a1114f311b41911fc
Michael <michael.daniels1013@gmail.com>
2022-12-21 23:14:04 -0600

    commit tree 2f992253efd2d5e8e5ff51caa43de1b3edc04df8

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,34 @@
+class StudentsController < ApplicationController
+
+    def index
+        render json: Student.all, status: 200
+    end
+
+    def show
+        render json: Student.find(params[:id]), status: 302 
+    end
+
+    def create
+        render json: Student.create!( student_params  )
+    end
+
+    def update
+        i = Student.find( params[:id] )
+        i.update!(student_params)
+        render json: i, status: :ok
+    end
+
+    def destroy
+        i = Student.find( params[:id] )
+        i.destroy
+        head :no_content
+    end
+
+
+    private
+
+    def student_params
+        params.permit( :name, :major, :age, :instructor_id )
+    end
+
+end
